3a44a92c33115606a482b664f77d5bd28439c889
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-02-28 09:14:58 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,10 @@
+#tabuada 
+
+numero=int(input('Digite um numero:'))
+multiplicador=1
+
+while(multiplicador <= 10):
+    resultado= numero * multiplicador
+
+    print(f'{numero} x {multiplicador}={resultado}')
+    multiplicador=multiplicador+1
