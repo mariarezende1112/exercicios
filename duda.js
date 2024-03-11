720731eacb121c488c8837b803320b3113db7baf
mariarezende1112 <131001998+mariarezende1112@users.noreply.github.com>
2024-03-11 09:03:15 -0300

    Add files via upload

---
+++ duda.js
@@ -0,0 +1,10 @@
+//Váriáveis representando idade e cidadania 
+let idade = 25;
+let possuiCárteiraDeMotorista = true;
+
+//Verificar se a idade é maior ou igal a 18 e se a cidadania é brasileira (true)
+
+if(idade>= 18 && possuiCárteiraDeMotorista){console.log ("pode dirigir");
+} else {
+    console.log("naõ pode dirigir");
+}
